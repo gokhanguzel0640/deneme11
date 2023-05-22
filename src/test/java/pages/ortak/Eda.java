70812bf1b4915b89c39b9a56a0b04dbd6d717145
EdaAltunay <altunay.eda1984@gmail.com>
2023-05-22 20:03:01 +0300

    eda.22.05.2023

--- src/test/java/pages/ortak/Eda.java
+++ src/test/java/pages/ortak/Eda.java
@@ -2,6 +2,8 @@ package pages.ortak;
 
 public class Eda {
     public static void main(String[] args) {
+
         System.out.println("eda");
+        System.out.println("Bu sefer oldu insallah :)");
     }
 }
