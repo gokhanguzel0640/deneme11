6f97dd9e781ce01c10f0392abb8894a8c7b1243a
Gokhan Guzel <126562774+gokhanguzel0640@users.noreply.github.com>
2023-05-22 13:39:06 +0300

    V1

---
+++ src/test/java/ortakClass.java
@@ -0,0 +1,11 @@
+public class ortakClass {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Zeynep sare");
+    }
+
+
+}
---
+++ src/test/java/pages/ortak/gokhan.java
@@ -0,0 +1,7 @@
+package pages.ortak;
+
+public class gokhan {
+    public static void main(String[] args) {
+        System.out.println("zeynep sare");
+    }
+}
